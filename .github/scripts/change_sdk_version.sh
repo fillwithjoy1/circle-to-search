978fd92abd11c8ea37a1f46bc8391f80334e7fc5
joysi <29514446+fillwithjoy1@users.noreply.github.com>
2024-10-31 13:45:05 +1000

    Fix later

--- .github/scripts/change_sdk_version.sh
+++ .github/scripts/change_sdk_version.sh
@@ -1,6 +1,18 @@
-#!/usr/bin/env sh
+mount -o rw,remount /system
+BUILD_PROP="/system/build.prop"
 
-# Won't be needed for this
-#BUILD_PROP = "/system/build.prop"
+if [ -e $BUILD_PROP ]
+then
+  if grep "ro.system.build.version.sdk=34" $BUILD_PROP
+  then
+    # TODO: Need to install Pixel Launcher 14 into the system
+    echo "Installing pixel_launcher 14"
+  else
+    sed -i -e 's/ro.system.build.version.sdk=33/ro.system.build.version.sdk=34/g' $BUILD_PROP
+    echo "Updated ro.system.build.version to 34! Be careful out there!"
+  fi
+else
+  echo "Can't find build.prop, aborting!"
+fi
 
-sed -i -e 's/ro.system.build.version.sdk=33/ro.system.build.version.sdk=34/g' build.prop
+mount -o ro,remount /system
