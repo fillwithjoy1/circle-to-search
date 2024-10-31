131ddf1c54966ee7e1ae018aa7ce25e1bf0cebcb
joysi <29514446+fillwithjoy1@users.noreply.github.com>
2024-10-31 14:05:22 +1000

    Fix later

--- .github/scripts/change_sdk_version.sh
+++ .github/scripts/change_sdk_version.sh
@@ -6,7 +6,7 @@ then
   if grep "ro.system.build.version.sdk=34" $BUILD_PROP
   then
     # TODO: Need to install Pixel Launcher 14 into the system
-    echo "Installing pixel_launcher 14"
+    echo "You are done! Install Pixel Launcher manually to finish installation"
   else
     sed -i -e 's/ro.system.build.version.sdk=33/ro.system.build.version.sdk=34/g' $BUILD_PROP
     echo "Updated ro.system.build.version to 34! Be careful out there!"
