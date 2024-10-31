40cd16e97b17d03d9049003b26ce14c386e775b6
joysi <29514446+fillwithjoy1@users.noreply.github.com>
2024-10-31 14:39:42 +1000

    Stuff

--- Module/customize.sh
+++ Module/customize.sh
@@ -6,13 +6,13 @@ then
   if grep "ro.system.build.version.sdk=34" $BUILD_PROP
   then
     # TODO: Need to install Pixel Launcher 14 into the system
-    echo "You are done! Install Pixel Launcher manually to finish installation"
+    ui_print "You are done! Install Pixel Launcher manually to finish installation"
   else
     sed -i -e 's/ro.system.build.version.sdk=33/ro.system.build.version.sdk=34/g' $BUILD_PROP
-    echo "Updated ro.system.build.version to 34! Be careful out there!"
+    ui_print "Updated ro.system.build.version to 34! Be careful out there!"
   fi
 else
-  echo "Can't find build.prop, aborting!"
+  ui_print "Can't find build.prop, aborting!"
 fi
 
 mount -o ro,remount /system
--- customize.sh
+++ customize.sh
@@ -1,24 +1,18 @@
-echo "Executing commands"
+mount -o rw,remount /system
+BUILD_PROP="/system/build.prop"
 
-# Execute commands needed to activate Circle to Search
-cmd device_config put launcher long_press_home_button_to_search true
+if [ -e $BUILD_PROP ]
+then
+  if grep "ro.system.build.version.sdk=34" $BUILD_PROP
+  then
+    # TODO: Need to install Pixel Launcher 14 into the system
+    ui_print "You are done! Install Pixel Launcher manually to finish installation"
+  else
+    sed -i -e 's/ro.system.build.version.sdk=33/ro.system.build.version.sdk=34/g' $BUILD_PROP
+    ui_print "Updated ro.system.build.version to 34! Be careful out there!"
+  fi
+else
+  ui_print "Can't find build.prop, aborting!"
+fi
 
-cmd device_config put launcher long_press_home_button_to_search_mpr true
-
-cmd device_config put launcher press_hold_nav_handle_to_search true
-
-cmd device_config put launcher press_hold_nav_handle_to_search_mpr true
-
-cmd device_config put launcher ENABLE_SETTINGS_OSE_CUSTOMIZATIONS true
-
-cmd device_config put launcher ENABLE_LONG_PRESS_NAV_HANDLE true
-
-cmd device_config put launcher ENABLE_LONG_PRESS_NAV_HANDLE_MPR true
-
-cmd device_config put launcher INVOKE_OMNI_LPH true
-
-cmd device_config put launcher INVOKE_OMNI_LPH_MPR true
-
-cmd device_config set_sync_disabled_for_tests persistent
-
-echo "Commands executed successfully! Reboot to to apply changes"
+mount -o ro,remount /system
