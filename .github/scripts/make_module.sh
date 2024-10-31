0a8b9bd92ed3fa7d7a3c5046e6a62048f9c7ec55
joysi <29514446+fillwithjoy1@users.noreply.github.com>
2024-10-31 14:58:07 +1000

    doubt

--- .github/scripts/make_module.sh
+++ .github/scripts/make_module.sh
@@ -6,10 +6,14 @@ sudo apt install zip
 mkdir module
 
 # We need to add META-INF, module.prop, customize.sh and uninstall.sh
+ls
 cp -r META-INF/ module
 cp module.prop module
 cp customize.sh module
 cp uninstall.sh module
+ls
+
+echo "HELLO?"
 
 zip -r module.zip module
 
