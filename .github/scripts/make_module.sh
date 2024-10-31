97381ec55b46c087bcf550abf8fcfeb40f2c696b
joysi <29514446+fillwithjoy1@users.noreply.github.com>
2024-10-31 14:57:04 +1000

    make_module.sh

--- .github/scripts/make_module.sh
+++ .github/scripts/make_module.sh
@@ -1,1 +1,16 @@
 #!/usr/bin/env bash
+
+sudo apt-get update -y && sudo apt upgrade -y
+sudo apt install zip
+
+mkdir module
+
+# We need to add META-INF, module.prop, customize.sh and uninstall.sh
+cp -r META-INF/ module
+cp module.prop module
+cp customize.sh module
+cp uninstall.sh module
+
+zip -r module.zip module
+
+exit
